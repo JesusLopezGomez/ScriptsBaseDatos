1c472eccda9ca9206a152fbd2515097e4a8088d9
lopeeh <113986224+lopeeh@users.noreply.github.com>
2023-05-15 18:18:18 +0200

    .

--- boletin6.sql
+++ boletin6.sql
@@ -2,93 +2,25 @@
 --y apellido de una persona, e inserte de forma automática esa persona como alumno.
 
 CREATE OR REPLACE 
-PROCEDURE Insertar_alumno(NOMBRE_P VARCHAR2, APELLIDO_P VARCHAR2) 
+PROCEDURE Insertar_alumno(NOMBRE_ALU VARCHAR2, APELLIDO_ALU VARCHAR2) 
 IS
-
-ID_ALUMNO VARCHAR2(40);
-DNI_ALUMNO VARCHAR2(40);
-
+	ID_ALU VARCHAR2(50);
+	DNI_ALU VARCHAR2(50);
 BEGIN
-	--Compruebo si el alumno existe
-	SELECT P.DNI INTO DNI_ALUMNO
+	SELECT P.DNI INTO DNI_ALU
 	FROM PERSONA p 
-	WHERE P.NOMBRE LIKE NOMBRE_P
-	AND P.APELLIDO LIKE APELLIDO_P;	
+	WHERE P.NOMBRE LIKE NOMBRE_ALU
+	AND P.APELLIDO LIKE APELLIDO_ALU;	
 
-	SELECT TO_CHAR(MAX(TO_NUMBER(SUBSTR(A.IDALUMNO,2))+1)) INTO ID_ALUMNO
+	SELECT TO_CHAR(MAX(TO_NUMBER(SUBSTR(A.IDALUMNO,2))+1)) INTO ID_ALU
 	FROM ALUMNO a;
 	
-	INSERT INTO ALUMNO a VALUES('A'||ID_ALUMNO,DNI_ALUMNO);
-	COMMIT;
-
-	EXCEPTION
-		WHEN NO_DATA_FOUND THEN
-			DBMS_OUTPUT.PUT_LINE('Esa persona no existe.');
-		WHEN OTHERS THEN
-			ROLLBACK;
-			DBMS_OUTPUT.PUT_LINE('Ha ocurrido un error' || SQLCODE || ' ' || SQLERRM);
+	INSERT INTO ALUMNO a VALUES('A'||ID_ALU,DNI_ALU);
+	
 END;
 
 BEGIN
 	Insertar_alumno('JESUS','LOPEZ');
-END;
-
-CREATE OR REPLACE 
-PROCEDURE Insertar_profesorS(NOMBRE_P VARCHAR2, APELLIDO_P VARCHAR2) 
-IS
-
-ID_PROFESOR VARCHAR2(40);
-DNI_ALUMNO VARCHAR2(40);
-
-BEGIN
-	--Compruebo si el profesor existe
-	SELECT P.DNI INTO DNI_ALUMNO
-	FROM PERSONA p 
-	WHERE P.NOMBRE LIKE NOMBRE_P
-	AND P.APELLIDO LIKE APELLIDO_P;	
-
-	SELECT TO_CHAR(MAX(TO_NUMBER(SUBSTR(p.IDPROFESOR ,2))+1)) INTO ID_PROFESOR
-	FROM PROFESOR p ;
-	
-	INSERT INTO PROFESOR P VALUES('p'||ID_PROFESOR,DNI_ALUMNO);
-	COMMIT;
-
-	EXCEPTION
-		WHEN NO_DATA_FOUND THEN
-			DBMS_OUTPUT.PUT_LINE('Esa persona no existe.');
-		WHEN OTHERS THEN
-			ROLLBACK;
-			DBMS_OUTPUT.PUT_LINE('Ha ocurrido un error' || SQLCODE || ' ' || SQLERRM);
-END;
-
-
---Crear un procedure para que se llame tres o más veces a la función anterior, mostrando 
---el mensaje “El alumno XXXX ha sido insertado”, o “El alumno XXXX no ha sido insertado, 
---y lo mismo con profesores donde XXXX será el dato concreto.
-
-CREATE OR REPLACE PROCEDURE UNIVERSIDAD.BUCLE_LLAMADA_3
-IS 
-
-numero number(5);
-
-BEGIN 
-	FOR I IN 1..3 LOOP 
-		
-		numero:= INSERTAR_ALUMNO_O_PROFESOR('JESUS','LOPEZ',2);
-	
-		IF numero = 1 THEN
-			DBMS_OUTPUT.PUT_LINE('El alumno XXXX ha sido insertado');
-		ELSIF numero = 2 THEN
-			DBMS_OUTPUT.PUT_LINE('El profesor XXXX ha sido insertado');
-		ELSE
-			DBMS_OUTPUT.PUT_LINE('El profesor o el profesor XXXX no ha sido insertado');
-		END IF;
-		
-	END LOOP;
-END;
-
-BEGIN
-	BUCLE_LLAMADA_3;
 END;
 
 --Realizar una función que reciba como parámetro el nombre y el apellido de una persona, 
@@ -97,58 +29,92 @@ END;
 --el código del profesor o alumno insertado. La función deberá devolver un 1 si se ha podido realizar 
 --la inserción, y un cero si ha ocurrido algún error.
 
-CREATE OR REPLACE FUNCTION UNIVERSIDAD.INSERTAR_ALUMNO_O_PROFESOR(V_NOMBRE VARCHAR2, V_APELLIDO VARCHAR2, NUMERO_INTRO NUMBER) 
-RETURN NUMBER
-IS 
-
-DNI_PERSONA VARCHAR2(20);
-ID_PROFESOR VARCHAR2(15);
-RETURN_NUM NUMBER(5) := 0;
-
-BEGIN 
-	--Compruebo si existe la persona
-	SELECT P.DNI INTO DNI_PERSONA
+CREATE OR REPLACE 
+FUNCTION INSERTAR_PROFESO(NOMBRE_PRO VARCHAR2, APELLIDO_PRO VARCHAR2) RETURN NUMBER
+IS
+	ID_PRO VARCHAR2(50);
+	DNI_ALU VARCHAR2(50);
+	NUM NUMBER:=1;
+BEGIN
+	--Compruebo si el profesor existe
+	SELECT P.DNI INTO DNI_ALU
 	FROM PERSONA p 
-	WHERE P.NOMBRE LIKE V_NOMBRE
-	AND P.APELLIDO LIKE V_APELLIDO;	
-	
-	IF NUMERO_INTRO = 1 THEN 	
-		RETURN_NUM := 1;
-		
-		Insertar_alumno(V_NOMBRE,V_APELLIDO);
-		
-		RETURN RETURN_NUM;	
-	
-	END IF;
+	WHERE P.NOMBRE LIKE NOMBRE_PRO
+	AND P.APELLIDO LIKE APELLIDO_PRO;	
 
-	IF NUMERO_INTRO = 2 THEN 
-		RETURN_NUM := 1;
-		
-		
-		Insertar_profesorS(V_NOMBRE,V_APELLIDO);	
-		
-		RETURN RETURN_NUM;
+	SELECT TO_CHAR(MAX(TO_NUMBER(SUBSTR(p.IDPROFESOR ,2))+1)) INTO ID_PRO
+	FROM PROFESOR p ;
+	
+	IF SQL%NOTFOUND THEN
+		NUM:=0;
 	END IF;
-	
+	INSERT INTO PROFESOR P VALUES('P'||ID_PRO,DNI_ALU);
+	RETURN NUM;
+END;
 
-	RETURN RETURN_NUM;
+--Crear un procedure para que se llame tres o más veces a la función anterior, mostrando 
+--el mensaje “El alumno XXXX ha sido insertado”, o “El alumno XXXX no ha sido insertado, 
+--y lo mismo con profesores donde XXXX será el dato concreto.
+
+CREATE OR REPLACE PROCEDURE EJER3
+IS 
+	NUM NUMBER;
+BEGIN 
+	FOR I IN 1..3 LOOP 
+		NUM:= INSERTAR_PROFESO('YERAY','HERRERIA');
 	
-	EXCEPTION
-		WHEN NO_DATA_FOUND THEN
-			DBMS_OUTPUT.PUT_LINE('Esa persona no existe.');
-		WHEN OTHERS THEN
-			DBMS_OUTPUT.PUT_LINE('Ha ocurrido un error' || SQLCODE || ' ' || SQLERRM);
-	
+		IF NUM = 1 THEN
+			DBMS_OUTPUT.PUT_LINE('El profesor XXXX ha sido insertado');
+		ELSE
+			DBMS_OUTPUT.PUT_LINE('El profesor XXXX no ha sido insertado');
+		END IF;
+	END LOOP;
+END;
+
+BEGIN
+	EJER3;
+END;
+
+--Realizar una función que reciba como parámetro el nombre y el apellido de una persona, 
+--también debe recibir un parámetro que podrá ser un 1 (en ese caso debe insertarlo como un alumno)
+-- o un 2 (debe insertarlo como profesor), y un parámetro de entrada salida en el que deberá devolver 
+--el código del profesor o alumno insertado. La función deberá devolver un 1 si se ha podido realizar 
+--la inserción, y un cero si ha ocurrido algún error.
+
+CREATE OR REPLACE FUNCTION ESTAMATRICULADO(DNI_V VARCHAR2, ASIGNATURA_V VARCHAR2, NOMBREAPELLIDOPROF IN OUT VARCHAR2) RETURN NUMBER 
+AS 
+	ID_ALU VARCHAR2(50);
+	ID_ASIG VARCHAR2(50);
+	ESTAMATRICULADO NUMBER;
+	NUM NUMBER:=0;
+BEGIN 
+    SELECT A.IDALUMNO INTO ID_ALU
+    FROM ALUMNO A
+    WHERE A.DNI = DNI_V;
+
+    SELECT A.IDASIGNATURA INTO ID_ASIG
+    FROM ASIGNATURA A 
+    WHERE A.NOMBRE LIKE ASIGNATURA_V;
+
+    SELECT COUNT(*) INTO ESTAMATRICULADO
+    FROM ALUMNO_ASIGNATURA AA 
+    WHERE AA.IDALUMNO LIKE ID_ALU AND AA.IDASIGNATURA LIKE ID_ASIG;
+
+    IF ESTAMATRICULADO>0 THEN
+	    SELECT P.NOMBRE || ' ' || P.APELLIDO INTO NOMBREAPELLIDOPROF
+	    FROM PERSONA P , PROFESOR P2 , ASIGNATURA A 
+	    WHERE P.DNI = P2.DNI AND P2.IDPROFESOR = A.IDASIGNATURA ;
+        NUM:=1;
+    END IF;
+    RETURN NUM;
+
 END;
 
 
 DECLARE 
 
-RETORNADO NUMBER(5);
-
 BEGIN
-	RETORNADO := INSERTAR_ALUMNO_O_PROFESOR('JESUS','LOPEZ',2);
-	DBMS_OUTPUT.PUT_LINE(RETORNADO);
+	DBMS_OUTPUT.PUT_LINE(ESTAMATRICULADO('17171717A','Contabilidad'));
 END;
 
 
@@ -288,3 +254,23 @@ END;
 INSERT INTO ASIGNATURA VALUES ('12345','Seguridad Via',2,1,60,'p307','130110',2);
 
 
+CREATE OR REPLACE FUNCTION SUBIDA_PRECIO(TITULACION_V VARCHAR2, PORCENTAJE NUMBER, SUBIDATOTAL IN OUT NUMBER) RETURN NUMBER 
+AS 
+	CURSOR ASIGNATURAS(NOM_TITULACION VARCHAR2)
+IS
+	SELECT A.COSTEBASICO AS COSTE
+	FROM ASIGNATURA A , TITULACION T 
+	WHERE T.NOMBRE = NOM_TITULACION AND T.IDTITULACION = A.IDASIGNATURA ;
+	CONTADOR NUMBER:=-1;
+BEGIN 
+    FOR ASIG IN ASIGNATURAS(TITULACION_V) LOOP
+        SUBIDATOTAL:=SUBIDATOTAL+(ASIG.COSTEPORCENTAJE);
+        UPDATE ASIGNATURA A SET A.COSTEBASICO = ASIG.COSTE + (ASIG.COSTEPORCENTAJE);
+        CONTADOR:=CONTADOR+1;
+    END LOOP;
+    
+   IF CONTADOR>-1 THEN
+    CONTADOR:=CONTADOR+1;
+    END IF;
+	RETURN CONTADOR;
+END;
